42fe8f97e8381ce076e7d931778ae881e2937046
Сергей Клевицкий <sergofun@MacBook-Pro.local>
2021-12-27 10:31:04 +0300

    initial

---
+++ lib/merkle_tree_root.ex
@@ -0,0 +1,18 @@
+defmodule MerkleTreeRoot do
+  @moduledoc """
+  Documentation for `MerkleTreeRoot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MerkleTreeRoot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/merkle_tree_root_test.exs
@@ -0,0 +1,8 @@
+defmodule MerkleTreeRootTest do
+  use ExUnit.Case
+  doctest MerkleTreeRoot
+
+  test "greets the world" do
+    assert MerkleTreeRoot.hello() == :world
+  end
+end
